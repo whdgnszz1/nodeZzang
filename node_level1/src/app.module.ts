3dfa6dc909f7f6a3c050d7ad7e51c85d93ede9a1
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-25 10:56:40 +0900

    Docs: Add comments module, controller, service to  project

--- node_level1/src/app.module.ts
+++ node_level1/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 import * as mongoose from 'mongoose';
 
 @Module({
@@ -12,6 +13,7 @@ import * as mongoose from 'mongoose';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     PostsModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
