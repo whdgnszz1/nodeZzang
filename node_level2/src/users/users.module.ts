ffce130f64ab807576c1573d18ab7d7574c2e7ef
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-25 18:27:04 +0900

    Feat: Implement UsersSchema to UsersModule
    1. src/users/users.module.ts
    - user schema를 injectModel로 사용하기 위해 users.module.ts에서 import함.

--- node_level2/src/users/users.module.ts
+++ node_level2/src/users/users.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
+import { Users, UsersSchema } from './users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
