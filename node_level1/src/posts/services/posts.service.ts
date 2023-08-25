afbdf4a3e2fb21b68ea99e781b475fadb575311e
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-25 12:39:28 +0900

    Feat: Implement createPost service
    1. src/posts/services/posts.service.ts
    - 게시글 생성 서비스 로직 작성
    - 게시글을 작성하기 위해 Post모델 주입
    
    2. src/posts/posts.module.ts
    - post service에서 post모델과 schema를 사용하기 위해 import

--- node_level1/src/posts/services/posts.service.ts
+++ node_level1/src/posts/services/posts.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PostRequestDto } from '../dto/posts.request.dto';
+import { Post } from '../posts.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectModel(Post.name) private readonly postModel: Model<Post>,
+  ) {}
+
+  async createPost(body: PostRequestDto) {
+    const { user, password, title, content } = body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const post = await this.postModel.create({
+      user,
+      title,
+      content,
+      password: hashedPassword,
+    });
+    return post.readOnlyData;
+  }
+}
