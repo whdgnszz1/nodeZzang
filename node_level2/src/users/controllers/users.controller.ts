dd596151bbcb7e9c7c6476e4bc71dbcf9371cd05
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-25 18:25:45 +0900

    Feat: Implement signup service to project
    1. src/users/controllers/users.controller.ts
    - @Body의 타입을 UsersRequestDto로 설정하여 nickname, password, confirmpassword를 받아서 user service에 전달하는 signUp함수 작성
    
    2. src/users/services/users.service.ts
    - users controller에서 받은 nickname, password, confirmPassord를 사용해 회원가입 절차를 검증하고 모두 통과된다면 회원가입하는 코드 작성

--- node_level2/src/users/controllers/users.controller.ts
+++ node_level2/src/users/controllers/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { UsersRequestDto } from '../dto/users.request.dto';
+import { UsersService } from '../services/users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @ApiOperation({ summary: '회원가입' })
+  @Post('signup')
+  signUp(@Body() body: UsersRequestDto) {
+    console.log(body);
+    return this.usersService.signUp(body);
+  }
+}
--- node_level2/src/users/services/users.service.ts
+++ node_level2/src/users/services/users.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UsersRequestDto } from '../dto/users.request.dto';
+import { Users } from '../users.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(Users.name) private readonly usersModel: Model<Users>,
+  ) {}
+
+  async signUp(body: UsersRequestDto) {
+    const { nickname, password, confirmPassword } = body;
+    const isNicknameExist = await this.usersModel.findOne({ nickname });
+    if (isNicknameExist) {
+      throw new HttpException('이미 존재하는 닉네임입니다.', 412);
+    }
+    if (password !== confirmPassword) {
+      throw new HttpException('패스워드가 일치하지 않습니다.', 412);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.usersModel.create({
+      nickname,
+      password: hashedPassword,
+    });
+    console.log(user);
+    return { message: '회원 가입에 성공하였습니다.' };
+  }
+}
