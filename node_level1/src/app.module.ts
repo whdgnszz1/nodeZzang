014a5cea4602ab253a7cf18acac99a22e4d86c6e
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-25 10:41:05 +0900

    Setting: Connect MongoDB to project

--- node_level1/src/app.module.ts
+++ node_level1/src/app.module.ts
@@ -1,11 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
