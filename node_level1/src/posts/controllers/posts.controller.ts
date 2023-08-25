7e2e8a926dc1c9210bcfc883bb69fb6a93b9f0d5
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-25 11:55:18 +0900

    Feat: Implement CRUD operations for posts API
    1. /posts/controllers/posts.controller.ts
    - 게시글에 대한 CRUD 엔드포인트 작성
    - @ApiOperation을 사용해 CRUD 엔드포인트에 대한 설명 추가

--- node_level1/src/posts/controllers/posts.controller.ts
+++ node_level1/src/posts/controllers/posts.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { PostRequestDto } from '../dto/posts.request.dto';
+import { PostsService } from '../services/posts.service';
 
 @Controller('posts')
-export class PostsController {}
+@UseInterceptors(SuccessInterceptor)
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @ApiOperation({ summary: '전체 게시글 조회' })
+  @Get()
+  getAllPosts() {
+    return 'get all posts';
+  }
+
+  @ApiOperation({ summary: '게시글 작성' })
+  @Post()
+  createPost(@Body() body: PostRequestDto) {
+    return this.postsService.createPost(body);
+  }
+
+  @ApiOperation({ summary: '게시글 상세 조회' })
+  @Get(':id')
+  getOnePost() {
+    return 'get one post';
+  }
+
+  @ApiOperation({ summary: '게시글 수정' })
+  @Put(':id')
+  updateOnePost() {
+    return 'update one post';
+  }
+
+  @ApiOperation({ summary: '게시글 삭제' })
+  @Delete(':id')
+  deletePost() {
+    return 'delete post';
+  }
+}
