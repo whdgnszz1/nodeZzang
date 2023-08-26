d10ba2264664463a701d8d42ae3c14138b921dac
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-26 13:06:49 +0900

    Setting: Setting TypeORM for project
    1. src/app.module.ts
    - TypeORM설치 후 app.module에 TypeOrmModule import

--- node_level2-3/src/app.module.ts
+++ node_level2-3/src/app.module.ts
@@ -9,15 +9,27 @@ import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import * as mongoose from 'mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     PostsModule,
     CommentsModule,
     UsersModule,
     AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
