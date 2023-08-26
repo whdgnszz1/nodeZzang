bb837221c2180361785777ea9b5ece2d330b9509
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-26 15:31:42 +0900

    Setting: Setting TypeORM for project
    1. src/app.module.ts
    - TypeORM을 사용하기 위해 app.module에 TypeOrmModule import함

--- node_level3/src/app.module.ts
+++ node_level3/src/app.module.ts
@@ -1,6 +1,6 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
@@ -8,16 +8,25 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import * as mongoose from 'mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
     PostsModule,
     CommentsModule,
     UsersModule,
     AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -26,6 +35,5 @@ export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
-    mongoose.set('debug', this.isDev);
   }
 }
