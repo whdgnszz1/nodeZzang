cfa6835e4ad1bc1724adc7b6cca25fc1623244dc
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-30 12:20:29 +0900

    Level3 과제 JWT를 통한 인증 시작

---
+++ node_level3_express_jwtLogin/src/controllers/comments.ts
@@ -0,0 +1,85 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  AllCommentResponse,
+  CreateCommentRequest,
+  UpdateCommentRequest,
+} from "../dtos/comments";
+import CommentsService from "../services/comments";
+
+// 댓글 생성
+export const createComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const postId = Number(req.params.postId);
+    const newComment: CreateCommentRequest = req.body;
+    await CommentsService.createComment(postId, newComment);
+    res.send({ message: "댓글을 생성하였습니다." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// 전체 댓글 조회
+export const getAllComments = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const allComments: AllCommentResponse[] =
+      await CommentsService.getAllComments();
+    res.json(allComments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// 특정 댓글 조회
+export const getOneComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const commentId: number = Number(req.params.commentId);
+    const comment = await CommentsService.getOneComment(commentId);
+    res.json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// 특정 댓글 수정
+export const updateOneComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const updateComment: UpdateCommentRequest = req.body;
+    const commentId: number = Number(req.params.commentId);
+    await CommentsService.updateOneComment(commentId, updateComment);
+    res.send({ message: "댓글을 수정하였습니다." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// 특정 댓글 삭제
+export const deleteOneComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { password } = req.body;
+    const commentId: number = Number(req.params.commentId);
+    const comment = await CommentsService.deleteOneComment(commentId, password);
+    res.send({ message: "댓글을 삭제하였습니다." });
+  } catch (error) {
+    next(error);
+  }
+};
