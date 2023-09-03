5425a9bf6113fe177cc52341f8d0ec64e9f26c08
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-03 21:37:50 +0900

    Feat: Register chat, profile, detail routers and check userLoggedinState

--- node_level4-5_express/frontend/src/shared/Router.tsx
+++ node_level4-5_express/frontend/src/shared/Router.tsx
@@ -1,13 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "src/pages/Login";
 import Main from "src/pages/Main";
+import Profile from "src/pages/Profile";
 import Signup from "src/pages/Signup";
+import Chat from 'src/pages/Chat'
+import Detail from "src/pages/Detail";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import { userState } from "src/states/userState";
+import Cookies from "js-cookie";
 const Router = () => {
+
+  const setIsLoggedIn = useSetRecoilState(userState);
+
+  useEffect(() => {
+    const token = Cookies.get("accessToken");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, [setIsLoggedIn]);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/detail/:postId" element={<Detail />} />
+
         <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
