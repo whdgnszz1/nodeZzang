725a5376ab7e29861e56adb66bf18b296252a05a
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-03 21:34:55 +0900

    Feat: Design PostCard and implement some feature
    좋아요기능, 상세게시글로 이동기능 추가

---
+++ node_level4-5_express/frontend/src/components/PostCard.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { postAPI } from "src/axios";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+
+type PostType = {
+  postId: number;
+  title: string;
+  content: string;
+  isLiked: boolean;
+};
+
+type Props = {
+  post: PostType;
+  onLike: any;
+};
+
+const PostCard: React.FC<Props> = ({ post, onLike }) => {
+  const navigate = useNavigate()
+
+  const handleLikeClick = async (event:React.MouseEvent) => {
+    event.stopPropagation()
+    try {
+      await postAPI(`/api/posts/${post.postId}/like`, {});
+      onLike(post.postId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCardClick = () => {
+    navigate(`/detail/${post.postId}`);
+  };
+
+  return (
+    <div onClick={handleCardClick} className="border-2 border-black h-[300px] flex flex-col relative">
+      <div className="border-b-2 border-black w-full h-10 flex items-center px-2">
+        {post.title}
+      </div>
+      <div className="w-full p-2">{post.content}</div>
+      <div className="absolute right-3 bottom-2">
+        {post.isLiked ? (
+          <span onClick={handleLikeClick}>
+            <AiFillHeart color="red" size={16} />
+          </span>
+        ) : (
+          <span onClick={handleLikeClick}>
+            <AiOutlineHeart />
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
