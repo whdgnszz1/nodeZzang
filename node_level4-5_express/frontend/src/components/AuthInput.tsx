8037085dce844b6452b1a4751f0c708a00aab364
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-03 21:28:06 +0900

    Modify: Modify AuthInput design

--- node_level4-5_express/frontend/src/components/AuthInput.tsx
+++ node_level4-5_express/frontend/src/components/AuthInput.tsx
@@ -10,18 +10,18 @@ const AuthInput: React.FC<Props> = ({ inputType, onInputChange }) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInputValue(value);
-    onInputChange(value); // 부모 컴포넌트로 입력값 전달
+    onInputChange(value);
   };
 
   return (
     <div className="flex gap-1">
-      <div className="w-[100px] text-1.25rem text-white font-semibold flex items-center">
+      <div className="w-[100px] text-1.25rem font-semibold flex items-center border-r-2 border-black">
         {inputType}
       </div>
       <input
         onChange={handleChange}
         type={inputType.includes("비밀번호") ? "password" : "text"}
-        className="rounded-xl px-4 py-1 text-start"
+        className="px-2 py-1 text-start border-black border-1 focus:border-none focus:outline-none"
       />
     </div>
   );
