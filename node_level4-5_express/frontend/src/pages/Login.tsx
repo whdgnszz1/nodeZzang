c8c4b6564d08a7189a471061f0d84a796401972a
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-03 21:35:34 +0900

    Feat: Design Login page and implement login feature

--- node_level4-5_express/frontend/src/pages/Login.tsx
+++ node_level4-5_express/frontend/src/pages/Login.tsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { postAPI } from "src/axios";
+import AuthInput from "src/components/AuthInput";
+import { useRecoilState } from "recoil";
+import { userState } from "src/states/userState";
+
 const Login = () => {
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [, setIsLoggedIn] = useRecoilState(userState);
+  const navigate = useNavigate();
+
+  const handleNicknameChange = (value: string) => {
+    setNickname(value);
+  };
+
+  const handlePasswordChange = (value: string) => {
+    setPassword(value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await postAPI("/api/login", {
+        nickname,
+        password,
+      }).then((data) => {
+        setIsLoggedIn(true);
+        console.log(data)
+      });
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <h1 className="text-7xl font-bold underline">로그인페이지</h1>
+      <div className=" h-screen flex justify-center items-center">
+        <div className="w-[768px] h-[1000px] border-2 border-black flex flex-col items-center justify-center gap-4">
+          <div className="border-2  w-[320px] border-black px-1">
+            <AuthInput
+              inputType="닉네임"
+              onInputChange={handleNicknameChange}
+            />
+          </div>
+          <div className="border-2  w-[320px] border-black px-1">
+            <AuthInput
+              inputType="비밀번호"
+              onInputChange={handlePasswordChange}
+            />
+          </div>
+
+          <button
+            onClick={handleSubmit}
+            className="bg-white w-[320px] font-semibold py-1 border-2 border-black"
+          >
+            로그인
+          </button>
+          <button className="bg-white w-[320px] font-semibold py-1 border-2 border-black">
+            카카오 로그인
+          </button>
+          <button className="bg-white w-[320px] font-semibold py-1 border-2 border-black">
+            구글 로그인
+          </button>
+        </div>
+      </div>
     </>
   );
 };
