0be6a24fd8529c04e3d666da18da59f811dff17a
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-05 14:36:08 +0900

    Design: Add icons to each tab

--- node_level4-5_express/frontend/src/components/Footer.tsx
+++ node_level4-5_express/frontend/src/components/Footer.tsx
@@ -1,17 +1,50 @@
-import { useNavigate } from "react-router-dom";
+import {
+  BsChatHeart,
+  BsChatHeartFill,
+  BsPerson,
+  BsPersonFill,
+  BsPostcardHeart,
+  BsPostcardHeartFill,
+} from "react-icons/bs";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Footer() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <div className="flex justify-around w-[768px] h-10 bg-rose-400 items-center text-white font-medium fixed bottom-0 border-x-2 border-t-2 border-black">
-      <div onClick={() => navigate("/")} className="cursor-pointer">
-        게시글
+      <div
+        onClick={() => navigate("/")}
+        className="cursor-pointer flex flex-col items-center"
+      >
+        {isActive("/") ? (
+          <BsPostcardHeartFill size={24} />
+        ) : (
+          <BsPostcardHeart size={24} />
+        )}
       </div>
-      <div onClick={() => navigate("/chat")} className="cursor-pointer">
-        채팅방
+      <div
+        onClick={() => navigate("/chat")}
+        className="cursor-pointer flex flex-col items-center"
+      >
+        {isActive("/chat") ? (
+          <BsChatHeartFill size={24} />
+        ) : (
+          <BsChatHeart size={24} />
+        )}
       </div>
-      <div onClick={() => navigate("/profile")} className="cursor-pointer">
-        내정보
+      <div
+        onClick={() => navigate("/profile")}
+        className="cursor-pointer flex flex-col items-center"
+      >
+        {isActive("/profile") ? (
+          <BsPersonFill size={24} />
+        ) : (
+          <BsPerson size={24} />
+        )}
       </div>
     </div>
   );
