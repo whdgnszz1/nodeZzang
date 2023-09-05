a73901a2804750a7f2993cf2eaf451f8b6f70601
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-05 14:37:50 +0900

    Feat: Add type and some chat feature

--- node_level4-5_express/frontend/src/pages/Chat.tsx
+++ node_level4-5_express/frontend/src/pages/Chat.tsx
@@ -6,32 +6,52 @@ import Navbar from "src/components/Navbar";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 
-interface ExtendedSocket extends Socket {
-  interval?: NodeJS.Timeout;
+/* 타입 지정 */
+interface ChatRoom {
+  _id: string;
+  title: string;
+  password: string;
+  maxMembers: number;
 }
 
+interface ChatRoomData {
+  title: string;
+  password: string;
+  maxMembers: string;
+}
+
+/* API 요청을 통해 채팅방 목록 가져오는 코드 */
+const fetchChatRooms = async (): Promise<ChatRoom[]> => {
+  const response = await getAPI("/api/chats/rooms");
+  return response.data.rooms;
+};
+
+/* Chat 컴포넌트 */
 function Chat() {
-  const [socket, setSocket] = useState<ExtendedSocket | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [chatRooms, setChatRooms] = useState<any[]>([]);
-  const [chatRoomData, setChatRoomData] = useState({
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+  const [chatRoomData, setChatRoomData] = useState<ChatRoomData>({
     title: "",
     password: "",
     maxMembers: "",
   });
 
-  const fetchChatRooms = async () => {
-    const { data } = await getAPI("/api/chats/rooms");
-    return data.rooms;
-  };
+  const modalRef = useRef<HTMLDivElement | null>(null);
 
-  const { data, isLoading, isError } = useQuery("chatRooms", fetchChatRooms);
+  /* 채팅방 가져와서 상태에 저장하는 코드 */
+  const { data, isLoading, isError } = useQuery<ChatRoom[]>(
+    "chatRooms",
+    fetchChatRooms
+  );
+
   useEffect(() => {
     if (data) {
       setChatRooms(data);
     }
   }, [data]);
 
+  /* 채팅방 목록을 가져오는 소캣에(room 네임스페이스) 연결하는 코드 */
   useEffect(() => {
     const newSocket = io(`${process.env.REACT_APP_SERVER_URL!}/room`, {
       path: "/socket.io",
@@ -43,11 +63,12 @@ function Chat() {
     };
   }, []);
 
+  /* 새로운 방이 생성됐을때 이벤트를 받고 추가하는 코드 */
   useEffect(() => {
     if (!socket) return;
 
-    socket.on("newRoom", (newRoom) => {
-      setChatRooms((prevRooms: any[]) => [...prevRooms, newRoom]);
+    socket.on("newRoom", (newRoom: ChatRoom) => {
+      setChatRooms((prevRooms: ChatRoom[]) => [...prevRooms, newRoom]);
     });
 
     return () => {
@@ -55,18 +76,17 @@ function Chat() {
     };
   }, [socket, setChatRooms]);
 
-  const modalRef = useRef<HTMLDivElement | null>(null);
-
+  /* 채팅방 생성시 모달, input값들 관리하는 코드 */
   const toggleModal = () => {
     setIsModalOpen((prev) => !prev);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
     setChatRoomData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleComplete = async () => {
+  const handleComplete = async (): Promise<void> => {
     try {
       await postAPI("/api/chats/rooms", chatRoomData);
       setChatRoomData({ title: "", password: "", maxMembers: "" });
@@ -76,19 +96,30 @@ function Chat() {
     }
   };
 
-  const handleClickOutside = useCallback((event: MouseEvent) => {
+  const handleClickOutside = useCallback((event: MouseEvent): void => {
     if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       toggleModal();
     }
   }, []);
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
+    document.addEventListener("keydown", (e: KeyboardEvent) => {
+      if (e.key === "Escape" && isModalOpen) {
         toggleModal();
       }
     });
+
+    return () => {
+      document.removeEventListener("keydown", (e: KeyboardEvent) => {
+        if (e.key === "Escape" && isModalOpen) {
+          toggleModal();
+        }
+      });
+    };
+  }, [handleClickOutside, isModalOpen]);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
@@ -106,9 +137,9 @@ function Chat() {
   return (
     <>
       <div className=" h-screen flex justify-center items-center">
-        <div className="relative w-[768px] h-[1000px] border-x-2 border-black flex flex-col items-center gap-4 justify-between overflow-auto px-2">
+        <div className="relative w-[768px] h-full border-x-2 border-black flex flex-col items-center gap-4 justify-between overflow-auto px-2">
           <Navbar />
-          <div className="w-full h-full flex flex-col mt-4">
+          <div className="w-full flex-1 mt-14 gap-2 overflow-auto">
             {chatRooms.map((room: any) => (
               <Link to={`/chat/${room._id}`} key={room._id}>
                 <div className="flex h-10 items-center border-b-2 border-black">
@@ -118,13 +149,13 @@ function Chat() {
             ))}
           </div>
           <button
-            className="absolute bottom-10 right-10 w-32 h-10 bg-rose-400 text-white rounded-md"
+            className="absolute bottom-12 right-10 w-32 h-10 bg-rose-400 text-white rounded-md"
             onClick={toggleModal}
           >
             채팅방 만들기
           </button>
           {isModalOpen && (
-            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-10">
+            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-30">
               <div ref={modalRef} className=" bg-white p-5 rounded-md">
                 <h2 className="mb-4 w-96">채팅방 만들기</h2>
                 <div className="mb-4">
