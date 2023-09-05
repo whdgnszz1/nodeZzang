776a1d6b7f1652f0a572e9d9936c65c05e1db61c
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-05 20:37:06 +0900

    Feat: Implement edit profile feature

--- node_level4-5_express/frontend/src/pages/Profile.tsx
+++ node_level4-5_express/frontend/src/pages/Profile.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { getAPI } from "src/axios";
+import EditProfileModal from "src/components/EditProfileModal";
 import Footer from "src/components/Footer";
 import Navbar from "src/components/Navbar";
 import PostCard from "src/components/PostCard";
+import { HiPencil } from "react-icons/hi";
 
 /* 타입 정의 */
 interface Post {
@@ -17,6 +19,7 @@ interface Post {
 interface User {
   userId: string;
   nickname: string;
+  profileUrl: string;
 }
 
 /* API 요청 */
@@ -28,7 +31,53 @@ const fetchPosts = async (): Promise<Post[]> => {
 
 /* Profile 컴포넌트 */
 function Profile() {
-  const user: User = JSON.parse(localStorage.getItem("user") as string);
+  const [user, setUser] = useState<User>(
+    JSON.parse(localStorage.getItem("user") as string)
+  );
+  const handleUserUpdate = (updatedUser: User) => {
+    setUser(updatedUser);
+  };
+  /* 모달창 관리 코드 */
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const handleEditIconClick = (e: any) => {
+    e.stopPropagation();
+    setIsEditModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsEditModalOpen(false);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (e: any) => {
+      if (e.target.closest(".modal-content")) return;
+      handleCloseModal();
+    };
+
+    window.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        handleCloseModal();
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
+  /* 모달창 관리 코드 끝 */
+
+  /* 좋아요누른 게시글 가져오는 코드 */
   const {
     data: posts = [],
     isLoading,
@@ -60,7 +109,7 @@ function Profile() {
   if (isError) {
     return <div>Error</div>;
   }
-
+  console.log(isEditModalOpen);
   return (
     <>
       <div className="h-screen min-h-screen flex justify-center items-center">
@@ -68,14 +117,23 @@ function Profile() {
           <Navbar />
           <div className="w-full h-full flex flex-col mt-20 gap-2">
             <div className="flex flex-col gap-4 border-b-2 border-black w-full justify-center items-center">
-              <div className="w-[200px] h-[200px] rounded-full">
+              <div className="relative w-[200px] h-[200px] rounded-full">
                 <img
-                  src={process.env.PUBLIC_URL + "/assets/default.png"}
+                  src={
+                    user.profileUrl
+                      ? user.profileUrl
+                      : process.env.PUBLIC_URL + "/assets/default.png"
+                  }
                   alt="user_profile"
                   className="rounded-full"
                 />
+                <div className="absolute bottom-[-8px] right-0 mb-2 mr-2 cursor-pointer">
+                  <div onClick={handleEditIconClick}>
+                    <HiPencil size={24} />
+                  </div>
+                </div>
               </div>
-              <div className="flex justify-center items-center text-xl font-semibold mb-4">
+              <div className="flex justify-center items-center text-xl font-semibold mb-4 cursor-default">
                 {user.nickname}
               </div>
             </div>
@@ -89,6 +147,12 @@ function Profile() {
           </div>
           <Footer />
         </div>
+        <EditProfileModal
+          isOpen={isEditModalOpen}
+          onClose={handleCloseModal}
+          user={user}
+          onUserUpdate={handleUserUpdate}
+        />
       </div>
     </>
   );
