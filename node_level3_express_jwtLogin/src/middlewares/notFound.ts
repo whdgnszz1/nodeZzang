1adcd7630be88cf782e6137bcdb7ffb1ba5b92f1
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-06 19:55:50 +0900

    Migrate node_Assignment to other repository

--- node_level3_express_jwtLogin/src/middlewares/notFound.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-const notFound = (req: Request, res: Response, next: NextFunction) => {
-    const error: any = new Error("Not Found");
-    error.status = 404;
-    next(error);
-};
-
-export default notFound;
