c8e24e091006cf620ae7ee32c40281f8ae9093f5
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-06 11:16:42 +0900

    Feat: Implement errormessage on text and refactor signup code

--- node_level4-5_express/frontend/src/pages/Signup.tsx
+++ node_level4-5_express/frontend/src/pages/Signup.tsx
@@ -1,55 +1,61 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { postAPI } from "src/axios";
+import { signUp } from "src/api/userAPI";
 import AuthInput from "src/components/AuthInput";
 
-
-const isValidNickname = (nickname: string) => {
-  // 최소 3자, 알파벳 대소문자(a~z, A~Z), 숫자(0~9)만 가능한 정규표현식
-  const regex = /^[a-zA-Z0-9]{3,}$/;
-  return regex.test(nickname);
-};
-
-const isValidPassword = (password: string, nickname: string) => {
-  // 최소 4자 이상이어야 하며, 닉네임과 같은 값이 포함되지 않아야 함
-  return password.length >= 4 && !password.includes(nickname);
-};
-
-
-
+type FormField = "nickname" | "password" | "confirm";
 
 const Signup = () => {
-  const [nickname, setNickname] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [confirm, setConfirm] = useState<string>("");
+  const [formData, setFormData] = useState({
+    nickname: "",
+    password: "",
+    confirm: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    nickname: null,
+    password: null,
+    confirm: null,
+  });
+
   const navigate = useNavigate();
 
-  /* 회원가입 로직 */
+  const validations = {
+    nickname: (value: string) => {
+      const regex = /^[a-zA-Z0-9]{3,}$/;
+      return regex.test(value)
+        ? null
+        : "닉네임은 최소 3자 이상, 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로만 구성해야 합니다.";
+    },
+    password: (value: string) => {
+      return value.length < 4 || value.includes(formData.nickname)
+        ? "비밀번호는 최소 4자 이상이어야 하며, 닉네임과 같은 값을 포함하면 안됩니다."
+        : null;
+    },
+    confirm: (value: string) => {
+      return value !== formData.password
+        ? "비밀번호와 비밀번호 확인 값이 일치하지 않습니다."
+        : null;
+    },
+  };
+
+  const handleChange = (field: FormField, value: string) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+    const error = validations[field](value);
+    setFormErrors((prev) => ({ ...prev, [field]: error }));
+  };
+
   const handleSubmit = async () => {
-    // 닉네임 유효성 검사
-    if (!isValidNickname(nickname)) {
-      alert("닉네임은 최소 3자 이상, 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로만 구성해야 합니다.");
+    const hasError = Object.values(formErrors).some((error) => error);
+
+    if (hasError) {
+      alert("입력값을 확인해주세요.");
       return;
     }
-  
-    // 비밀번호와 비밀번호 확인 값이 일치하는지 검사
-    if (password !== confirm) {
-      alert("비밀번호와 비밀번호 확인 값이 일치하지 않습니다.");
-      return;
-    }
-  
-    // 비밀번호 유효성 검사
-    if (!isValidPassword(password, nickname)) {
-      alert("비밀번호는 최소 4자 이상이어야 하며, 닉네임과 같은 값을 포함하면 안됩니다.");
-      return;
-    }
-  
+
     try {
-      await postAPI("/api/signup", {
-        nickname,
-        password,
-        confirm,
-      });
+      const { nickname, password, confirm } = formData;
+      await signUp({ nickname, password, confirm });
       navigate("/login");
     } catch (error: any) {
       console.error(error);
@@ -57,18 +63,34 @@ const Signup = () => {
     }
   };
 
+  const inputFields = [
+    { key: "nickname" as FormField, label: "닉네임" },
+    { key: "password" as FormField, label: "비밀번호" },
+    { key: "confirm" as FormField, label: "비밀번호 확인" },
+  ];
+
   return (
     <div className="h-screen min-h-screen flex justify-center items-center">
       <div className="border-black border-2 w-[768px] h-[1000px] flex flex-col items-center justify-center gap-4">
-        <div className="border-2 w-[320px] border-black px-1">
-          <AuthInput inputType="닉네임" onInputChange={setNickname} />
-        </div>
-        <div className="border-2 w-[320px] border-black px-1">
-          <AuthInput inputType="비밀번호" onInputChange={setPassword} />
-        </div>
-        <div className="border-2 w-[320px] border-black px-1">
-          <AuthInput inputType="비밀번호 확인" onInputChange={setConfirm} />
-        </div>
+        {inputFields.map((field) => (
+          <div className="flex flex-col justify-center items-center gap-2 ">
+            <div
+              key={field.key}
+              className="border-2 w-[320px] border-black px-1"
+            >
+              <AuthInput
+                inputType={field.label}
+                onInputChange={(value) => handleChange(field.key, value)}
+              />
+            </div>
+            {formErrors[field.key] && (
+              <span className="text-red-500 text-xs">
+                {formErrors[field.key]}
+              </span>
+            )}
+          </div>
+        ))}
+
         <button
           onClick={handleSubmit}
           className="bg-white w-[320px] border-2 font-semibold border-black py-1"
@@ -76,13 +98,13 @@ const Signup = () => {
           회원가입 완료
         </button>
         <button
-          onClick={() => navigate('/login')}
+          onClick={() => navigate("/login")}
           className="bg-white w-[320px] border-2 font-semibold border-black py-1"
         >
           로그인 창으로
         </button>
         <button
-          onClick={() => navigate('/')}
+          onClick={() => navigate("/")}
           className="bg-white w-[320px] border-2 font-semibold border-black py-1"
         >
           홈으로
