136ee85be898782b94a3f5ffb982ed7775ef55b8
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-08 15:19:42 +0900

    Fix: Fix googleStrategy

--- nodeZzang/backend/src/passport/googleStrategy.ts
+++ nodeZzang/backend/src/passport/googleStrategy.ts
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import prisma from "../utils/prisma";
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
 export default () => {
   passport.use(
@@ -10,9 +10,8 @@ export default () => {
         clientID: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         callbackURL: `${process.env.REDIRECT_URL}/api/google/callback`,
-        passReqToCallback: true,
       },
-      async (req, accessToken, refreshToken, profile, done) => {
+      async (accessToken, refreshToken, profile, done) => {
         try {
           const existUser = await prisma.users.findUnique({
             where: {
@@ -21,48 +20,47 @@ export default () => {
           });
 
           if (existUser) {
-            const googleLoggedInToken = jwt.sign(
-              { userId: existUser.userId, nickname: existUser.nickname },
-              process.env.JWT_SECRET!,
-              { expiresIn: "1h" }
-            );
-            req.cookies.googleLoggedInToken = googleLoggedInToken;
+            // const googleLoggedInToken = jwt.sign(
+            //   { userId: existUser.userId, nickname: existUser.nickname },
+            //   process.env.JWT_SECRET!,
+            //   { expiresIn: "1h" }
+            // );
 
-            done(null, existUser);
-          } else {
-            let nickname = profile.displayName;
-            let count = 1;
-
-            while (
-              await prisma.users.findUnique({ where: { nickname: nickname } })
-            ) {
-              nickname = `${profile.displayName}${count}`;
-              count++;
-            }
-
-            const newUser = await prisma.users.create({
-              data: {
-                email: profile.emails
-                  ? profile.emails[0].value
-                  : "default@gmail.com",
-                nickname: nickname,
-                snsId: profile.id,
-                provider: "google",
-              },
-            });
-
-            const googleLoggedInToken = jwt.sign(
-              { userId: newUser.userId, nickname: newUser.nickname },
-              process.env.JWT_SECRET!,
-              { expiresIn: "1h" }
-            );
-            req.cookies.googleLoggedInToken = googleLoggedInToken;
-
-            done(null, newUser);
+            return done(null, existUser);
           }
+
+          let nickname = profile.displayName;
+          let count = 1;
+
+          const existingNicknameUser = await prisma.users.findUnique({
+            where: { nickname: nickname },
+          });
+          while (existingNicknameUser) {
+            nickname = `${profile.displayName}${count}`;
+            count++;
+          }
+
+          const newUser = await prisma.users.create({
+            data: {
+              email: profile.emails
+                ? profile.emails[0].value
+                : "default@gmail.com",
+              nickname: nickname,
+              snsId: profile.id,
+              provider: "google",
+            },
+          });
+
+          // const googleLoggedInToken = jwt.sign(
+          //   { userId: newUser.userId, nickname: newUser.nickname },
+          //   process.env.JWT_SECRET!,
+          //   { expiresIn: "1h" }
+          // );
+
+          return done(null, newUser);
         } catch (error: any) {
-          console.error(error);
-          done(error);
+          console.error("Error during Google authentication:", error);
+          return done(error);
         }
       }
     )
