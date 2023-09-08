a54c321a2800c9e7317fbb2a903474c0005daa9c
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-08 16:59:07 +0900

    Chore: Remove unused code

--- nodeZzang/backend/src/passport/googleStrategy.ts
+++ nodeZzang/backend/src/passport/googleStrategy.ts
@@ -1,7 +1,6 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import prisma from "../utils/prisma";
-// import jwt from "jsonwebtoken";
 
 export default () => {
   passport.use(
